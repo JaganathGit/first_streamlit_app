364717381bbeb54b0bd13abcf4016fb247cecd46
Jaganath <119089094+JaganathGit@users.noreply.github.com>
2022-11-26 08:19:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Dosa')
+streamlit.text('Masala Dosa')
+streamlit.text('Idly')
